272b07f1e20b809d4a8ee20df0faf7f54d3fada4
aliAxDev <aliaxdev000@gmail.com>
2023-09-12 20:45:41 -0400

    2 Comint - cambios

---
+++ 1-Pomodoro Lista Tareas Cronometro/index.js
@@ -0,0 +1,7 @@
+const tareas = [];
+const duracion = null;
+const descanso = null;
+const intervalo = null;
+
+const tarea_en_curso = document.querySelector('.tarea-en-curso');
+const form = document.querySelector('.form');
