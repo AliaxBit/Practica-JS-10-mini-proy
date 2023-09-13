dbd1502b4cb527f0ae60fe15c8d204fa9429316d
aliAxDev <aliaxdev000@gmail.com>
2023-09-12 20:57:31 -0400

    Update index.js
    3er commit

--- 1-Pomodoro Lista Tareas Cronometro/index.js
+++ 1-Pomodoro Lista Tareas Cronometro/index.js
@@ -5,3 +5,4 @@ const intervalo = null;
 
 const tarea_en_curso = document.querySelector('.tarea-en-curso');
 const form = document.querySelector('.form');
+const intro = document.querySelector('.intro');
