4e0cfc44e7d46071e3e990ee2571a3fa983c8107
AliaxDev <estupi1968@gmail.com>
2023-09-21 23:25:19 -0400

    1er Commit Compelto!

---
+++ 9-Componente de Etiquetas/script.js
@@ -0,0 +1,84 @@
+
+let tags = [];
+const inputTagContainer = document.querySelector('#input_tag');
+const tagsContainer = document.createElement('div');
+const inputTag = document.createElement('span');
+
+inputTag.ariaRoleDescription = 'textbox';
+inputTag.contentEditable = 'true';
+inputTag.classList.add('input');
+inputTag.focus();
+
+inputTagContainer.classList.add('input_tags_container');
+tagsContainer.classList.add('tag_container');
+
+inputTagContainer.appendChild(tagsContainer);
+tagsContainer.appendChild(inputTag);
+
+inputTagContainer.addEventListener('click', (e) => {
+
+    if (
+        e.target.id === 'input_tag' ||
+        e.target.classList.contains('tag_container')
+    ) {
+        inputTag.focus();
+    }
+});
+
+inputTag.addEventListener('keydown', (e) => {
+
+    if (e.key === 'Enter' && inputTag.textContent != '') {
+        e.preventDefault();
+        if (!existTag(inputTag.textContent)) {
+            tags.push(inputTag.textContent);
+            inputTag.textContent = "";
+            removeTag();
+        }
+    } else if (
+        e.key == "Backspace" &&
+        inputTag.textContent == ""
+        && tags.length > 0
+    ) {
+        tags.pop();
+        renderTags();
+    }
+});
+
+function renderTags() {
+    tagsContainer.innerHTML = "";
+
+    const html = tags.map(tag => {
+
+        const tagElement = document.createElement('div');
+        const tagButton = document.createElement('button');
+
+        tagElement.classList.add('tag_item');
+        tagButton.textContent = 'x';
+        tagButton.addEventListener('click', (e) => {
+
+            removeTag(tag);
+        });
+        tagElement.appendChild(document.createTextNode(tag));
+        tagElement.appendChild(tagButton);
+
+        return tagElement;
+    });
+
+    html.forEach(element => {
+
+        tagsContainer.appendChild(element);
+    });
+    tagsContainer.appendChild(inputTag);
+    inputTag.focus();
+
+}
+
+function existTag(value) {
+
+    return tags.includes(value);
+}
+
+function removeTag(value) {
+    tags = tags.filter(tag => tag != value);
+    renderTags();
+}
